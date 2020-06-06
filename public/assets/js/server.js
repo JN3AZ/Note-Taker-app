ac94e6d152d3b1b3a18a4de031f3c99800e83291
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-06 01:31:45 -0700

    added my .get methods, three of them for /notes, /api/notes, and for the wildcard *, wrote a .sendFile response for /notes and *, wrote an fs.readFile method for the /api/notes and added the .send of the json type response

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -9,3 +9,19 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extend: true}));
 app.use(express.json());
+
+  //Get APIs 
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+
+  app.get("/api/notes", function(req, res) {
+    fs.readFile('db.json', (err, data) => {
+        if (err) throw err;
+        res.type('json');
+        res.send(data);
+      });
+  });
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
