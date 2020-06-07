1666a726d75f63c6d29b019dd0ad5ddfc0b3fabc
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-07 16:18:01 -0700

    finished writing out delete method

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-//setting up newly learned express dependency 
+//setting up newly learned express dependency
 const app = express();
 // const PORT = 8080;
 
@@ -11,53 +11,65 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //these two lines are needed to properly parse data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-  //Get APIs 
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-  });
-
-  app.get("/api/notes", function(req, res) {
-    fs.readFile('db.json', (err, data) => {
-        if (err) throw err;
-        res.type('json');
-        res.send(data);
-      });
-  });
-  //this sends any wildcard requests to the index.html page
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
-
-   //Post APIs
-   app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    let rawData = fs.readFileSync('./db/db.json');
-    // let student = JSON.parse(rawdata);
-    // [{"title":"Test Title","text":"Test text"}]
-    let notesArray;
-
-    notesArray = JSON.parse(rawData);
-    // console.log(newNote);
-    // console.log(notesArray);
-    // console.log(notesArray.constructor);  
-    // console.log(typeof notesArray);
-    // console.log(notesArray.length);
-    notesArray.push(newNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notesArray));
-    
-    res.json(newNote);
+//Get APIs
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-   //delete APIs
-    app.delete("/api/notes/:id", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
-
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db.json", (err, data) => {
+    if (err) throw err;
+    res.type("json");
+    res.send(data);
   });
+});
 
-//this listen method allows computer to hear and execute user requests 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+//this sends any wildcard requests to the index.html page
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//Post APIs
+app.post("/api/notes", function (req, res) {
+  let newNote = req.body;
+  let rawData = fs.readFileSync("./db/db.json");
+  // let student = JSON.parse(rawdata);
+  // [{"title":"Test Title","text":"Test text"}]
+  let notesArray;
+
+  notesArray = JSON.parse(rawData);
+  // console.log(newNote);
+  // console.log(notesArray);
+  // console.log(notesArray.constructor);
+  // console.log(typeof notesArray);
+  // console.log(notesArray.length);
+  notesArray.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesArray));
+
+  res.json(newNote);
+});
+
+//delete APIs
+app.delete("/api/notes/:id", function (req, res) {
+  // res.sendFile(path.join(__dirname, "./db/db.json"));
+  let notesArray = JSON.parse(fs.readFileSync("./db/db.json"));
+  let removeId = req.params.id;
+  // console.log(notesArray.length);
+  //established a For loop in order to pass over each json in db array to find index of req id to be deleted
+  for (let i = 0; i < notesArray.length; i++) {
+    if (notesArray[i].id === removeId) {
+      //using splice method to remove specific note from db array
+      notesArray.splice(i, 1);
+      //write updated notesArray to db json array
+      fs.writeFileSync("./db/db.json", JSON.stringify(notesArray));
+    }
+  }
+});
+
+//this listen method allows computer to hear and execute user requests
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
